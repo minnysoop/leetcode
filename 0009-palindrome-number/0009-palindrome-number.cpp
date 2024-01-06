125ede8b918e03c640686f5a3aeff06d34dc3b46
Min Soo Kang <135174995+minnysoop@users.noreply.github.com>
2024-01-06 11:24:47 +0900

    Time: 7 ms (74.83%), Space: 6.4 MB (20.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+        bool isPalindrome(int x) {
+            if (x < 0 || (x != 0 && x % 10 == 0)){
+                return false;
+            }
+
+            int reversed_num = 0;
+            while (x > reversed_num){
+                reversed_num = reversed_num * 10 + x % 10;
+                x = x/10;
+            }
+
+            return (x == reversed_num || x == reversed_num/10);
+
+        }
+};
