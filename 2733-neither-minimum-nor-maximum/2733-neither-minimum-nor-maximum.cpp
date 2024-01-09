652cffd8f4214a5dceb8d75ebda0dd6c5281af6b
Min Soo Kang <135174995+minnysoop@users.noreply.github.com>
2024-01-09 19:33:42 +0900

    Time: 87 ms (65.63%), Space: 92 MB (89.47%) - LeetHub

---
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        if (nums.size() < 3){
+            return -1;
+        }
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+    }
+};
