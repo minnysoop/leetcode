557764105e0f57fc07686ebcbe890fbd61783d3d
Min Soo Kang <135174995+minnysoop@users.noreply.github.com>
2024-01-10 15:59:03 +0900

    Time: 7 ms (55.47%), Space: 20.7 MB (67.30%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int n = nums.size();
+        int c = 0;
+        for (int i=0;i<n-1;i++){
+            for (int j=i+1;j<n;j++){
+                if (nums[i] + nums[j] < target){
+                    c += 1;
+                }
+            }
+        }
+        return c;
+    }
+};
