cd94531d4ef535065bf6ab9bbae0bce16c26a944
Min Soo Kang <135174995+minnysoop@users.noreply.github.com>
2024-01-26 17:59:40 +0900

    Time: 0 ms (100.00%), Space: 9 MB (51.46%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1){
+            return nums[0];
+        }
+        
+        int a = nums[0];
+        int b = max(nums[0], nums[1]);
+        for (int i=2;i<n;i++){
+            int c = max(b, a + nums[i]);
+            a = b;
+            b = c;
+        }
+        
+        return max(a, b);
+    }
+};
