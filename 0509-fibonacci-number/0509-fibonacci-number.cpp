4b74ce21aa53ae56ddc2091efb9437b99c282a85
Min Soo Kang <135174995+minnysoop@users.noreply.github.com>
2024-01-26 14:25:52 +0900

    Time: 0 ms (100.00%), Space: 7.1 MB (8.20%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n < 2){
+            return n;
+        }
+        
+        int a = 0;
+        int b = 1;
+        int c;
+        for (int i=2;i<=n;i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        
+        return c;
+    }
+};
