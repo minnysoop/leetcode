2f325e352a3eda07056144748c3e0144cbd3173e
Min Soo Kang <135174995+minnysoop@users.noreply.github.com>
2024-01-26 15:49:01 +0900

    Time: 2 ms (46.51%), Space: 7 MB (5.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2){
+            return n;
+        }
+        
+        int steps[n];
+        steps[0] = 1;
+        steps[1] = 2;
+        
+        for (int i=2;i<n;i++){
+            steps[i] = steps[i-1] + steps[i-2];
+        }
+        
+        return steps[n-1];
+    }
+};
