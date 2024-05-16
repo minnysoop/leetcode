513fd02f051064e3130e7f8dc6248a9646611b58
Min Soo Kang (강민수) <135174995+minnysoop@users.noreply.github.com>
2024-05-15 19:44:57 -0500

    Time: 8 ms (8.10%), Space: 11.2 MB (5.06%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     bool isPathCrossing(string path) {
         vector<pair<int, int>>vp;
-        
         pair<int,int>p;
         p.first = 0;
         p.second = 0;
-        
         vp.push_back(p);
-        
         int n = path.size();
         for (int i=0;i<n;i++){
             if (path[i] == 'N') {
@@ -26,7 +23,6 @@ public:
             
             vp.push_back(p);
         }
-        
         set<pair<int,int>>s;
         for (int i=0;i<vp.size();i++){
             s.insert(vp[i]);
